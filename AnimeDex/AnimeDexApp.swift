1b0ed078ee346d6288c6501d8a5cca3a615c61d7
romanuriel <romanufrancisicomartinez@gmail.com>
2025-01-22 10:53:49 -0600

    Initial Commit

---
+++ AnimeDex/AnimeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeDexApp.swift
+//  AnimeDex
+//
+//  Created by Roman Uriel on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeDexTests/AnimeDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeDexTests.swift
+//  AnimeDexTests
+//
+//  Created by Roman Uriel on 22/01/25.
+//
+
+import Testing
+@testable import AnimeDex
+
+struct AnimeDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
